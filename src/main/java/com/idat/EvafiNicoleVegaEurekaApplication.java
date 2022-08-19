7c0928daa61475fbf5226bf11981e38fa95c97c9
nicolemel <a19102346@idat.edu.pe>
2022-08-18 23:35:41 -0500

    Eureka

---
+++ src/main/java/com/idat/EvafiNicoleVegaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvafiNicoleVegaEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvafiNicoleVegaEurekaApplication.class, args);
+	}
+
+}
